Fixing an error on copying for maze domain, also normalising it

Envs/MazeEnv.py
@@ -54,7 +54,7 @@ class MazeEnv(gym.Env):
         self.made_screen = False
         self.pellets = (self.maze == 2).sum()
 
-    def __init__(self, size=(10, 10), seed=2):
+    def __init__(self, size=(10, 10), seed=2, normalise=True):
         self.actions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
         self.maze = np.zeros(shape=size)
         self.seed = seed
@@ -64,7 +64,7 @@ class MazeEnv(gym.Env):
 
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low=0, high=3, shape=size)
-        self.reward_range = (-1, 10)
+        self.reward_range = (-6, 9)
 
     def _step(self, a):
         self.steps += 1
@@ -87,12 +87,12 @@ class MazeEnv(gym.Env):
         if self.steps >= self.limit:
             finished = True
 
-        return self.maze[:, :, np.newaxis], r, finished, {}
+        return self.maze[:, :, np.newaxis] / 3, r / 9, finished, {}
 
     def _reset(self):
         self.reset_maze()
         self.steps = 0
-        return self.maze[:, :, np.newaxis]
+        return self.maze[:, :, np.newaxis] / 3
 
     def _render(self, mode="human", close=False):
         if close:

Models/DQN_Maze.py
@@ -5,9 +5,11 @@ import tflearn
 def model(name="Model", actions=2):
     with tf.name_scope(name):
         inputs = tf.placeholder(tf.float32, shape=[None, 10, 10, 1], name="Observation_Input")
+
+        # Truncated normal (Defauly init for tflearn) is good enough
         net = tflearn.fully_connected(inputs, 256, activation="relu", name="FC1")
         net = tflearn.fully_connected(net, 128, activation="relu", name="FC2")
-        net = tflearn.fully_connected(net, 64, activation="relu", name="FC3")
+        # net = tflearn.fully_connected(net, 128, activation="relu", weights_init=w_init, bias_init=b_init, name="FC1")
         q_values = tflearn.fully_connected(net, actions, activation="linear")
 
         action_index = tf.placeholder(tf.float32, shape=[None, actions])

Replay/ExpReplay.py
@@ -10,7 +10,8 @@ class ExperienceReplay:
     def Add_Exp(self, state_now, action, reward, state_after, terminal):
         if len(self.Exps) >= self.N:
             self.Exps.pop(0)
-        self.Exps.append((state_now, action, reward, state_after, terminal))
+        # Make copies just in case
+        self.Exps.append((np.copy(state_now), action, reward, np.copy(state_after), terminal))
 
     def Sample(self, size):
         assert(size <= self.N)