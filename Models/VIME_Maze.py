5913aed0ccb63f6b9f54412fb52041bf18070b80
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-01-12 17:26:31 +0000

    Vime maze model

--- Models/VIME_Maze.py
+++ Models/VIME_Maze.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from Bayesian.Bayesian_Conv_Layer import Bayesian_Conv
 from Bayesian.Bayesian_DeConv_Layer import Bayesian_DeConv
 from Bayesian.Bayesian_FC_Layer import Bayesian_FC
-from Bayesian.Bayesian_Net import Bayesian_Net
+from Bayesian.Bayesian_Net import Bayesian_Net, log_gaussian_pdf
 from math import ceil
 
 
@@ -10,6 +10,8 @@ def model(name="Exploration_Model", size=1, actions=4):
     with tf.name_scope(name):
         inputs = tf.placeholder(tf.float32, shape=[None, size * 7, size * 7, 1], name="Observation_Input")
         action = tf.placeholder(tf.float32, shape=[None, actions])
+        target = tf.placeholder(tf.float32, shape=[None, size * 7, size * 7, 1], name="Observation_Input")
+        kl_scaling = tf.placeholder(tf.float32, shape=[])
         img_size = 7 * size
 
         l1 = Bayesian_Conv(1, 8, filter_height=3, filter_width=3, filter_stride=2)
@@ -39,41 +41,22 @@ def model(name="Exploration_Model", size=1, actions=4):
             net = l5.sample(net, local_reparam_trick)
             return net
 
+        def bnet_prob(pred, target):
+            return log_gaussian_pdf(pred, target, 0.1)
 
-        # Truncated normal (Defauly init for tflearn) is good enough
-        net = tflearn.fully_connected(inputs, 256, activation="relu", name="FC1")
-        net = tflearn.fully_connected(net, 128, activation="relu", name="FC2")
-        # net = tflearn.fully_connected(net, 128, activation="relu", weights_init=w_init, bias_init=b_init, name="FC1")
-        q_values = tflearn.fully_connected(net, actions, activation="linear")
+        bayesian_net = Bayesian_Net([l1, l2, l3, l4, l5], bnet_prob)
 
-        action_index = tf.placeholder(tf.float32, shape=[None, actions])
-        target_q = tf.placeholder(tf.float32, shape=[None, actions])
-        q_error = action_index * (q_values - target_q)
-        q_loss = tf.reduce_mean(tf.square(q_error))
-
-        variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=name)
-
-        # Summaries
-        loss_summary = tf.summary.scalar("Q_Loss", q_loss)
-        qval_summaries = []
-        for i in range(actions):
-            qval_summaries.append(tf.summary.scalar("Action {} QValue".format(i), q_values[0, i]))
-        average_qval = tf.reduce_mean(q_values)
-        avg_qvals_summary = tf.summary.scalar("Average QValue", average_qval)
-        qvals_histogram = tf.summary.histogram("Q Values", q_values)
-        qval_summaries.append(qvals_histogram)
-        qval_summaries.append(avg_qvals_summary)
-        qvals_summary = tf.summary.merge(qval_summaries)
+        bnet_loss, kl_loss, data_loss = bayesian_net.loss(sample, target, kl_scaling=kl_scaling, N=8)
+        bnet_output = sample(local_reparam_trick=False)
 
     dict = {}
     dict["Input"] = inputs
-    dict["Q_Values"] = q_values
-    dict["Q_Loss"] = q_loss
-    dict["Variables"] = variables
-    dict["Targets"] = target_q
-    dict["Actions"] = action_index
-    dict["Loss_Summary"] = loss_summary
-    dict["QVals_Summary"] = qvals_summary
+    dict["Output"] = bnet_output
+    dict["Target"] = target
+    dict["Action"] = action
+    dict["Loss"] = bnet_loss
+    dict["KL_Loss"] = kl_loss
+    dict["Data_Loss"] = data_loss
 
     return dict
 
