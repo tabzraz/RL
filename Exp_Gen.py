6fdda5af9f3d02137a8b328ddba4692ca0d370bc
Tabz <tabzraz@gmail.com>
2017-03-10 03:36:25 +0000

    .

--- Exp_Gen.py
+++ Exp_Gen.py
@@ -5,6 +5,7 @@ betas = [0.01]
 t_maxs = [500000]
 cts_sizes = [7]
 seeds = [13, 66, 99]
+epsilon_starts = [0.1]
 
 gpu = True
 debug_eval = True
@@ -14,27 +15,28 @@ uid = 0
 for env in envs:
     for t_max in t_maxs:
         for lr in lrs:
-            for count in counts:
-                for beta in betas:
-                    for cts_size in cts_sizes:
-                        for seed in seeds:
-                            name = env.replace("-", "_")[:-3]
-                            if count:
-                                name += "_Count_Cts_{}_Beta_{}_uid_{}".format(cts_size, beta, uid)
-                            else:
-                                name += "_DQN_uid_{}".format(uid)
-                            python_command = "python3 RL_Trainer_PyTorch.py --name {} --env {} --lr {} --seed {} --t-max {}".format(name, env, lr, seed, t_max)
-                            if count:
-                                python_command += " --count --beta {} --cts-size {}".format(beta, cts_size)
-                            if gpu:
-                                python_command += " --gpu"
-                            if debug_eval:
-                                python_command += " --debug-eval"
-                            if screen:
-                                screen_name = "DQN"
+            for eps in epsilon_starts:
+                for count in counts:
+                    for beta in betas:
+                        for cts_size in cts_sizes:
+                            for seed in seeds:
+                                name = env.replace("-", "_")[:-3]
                                 if count:
-                                    screen_name = "Count"
-                                screen_name += "_{}".format(seed)
-                                python_command = "screen -mdS {}__{} bash -c \"{}\"".format(uid, screen_name, python_command)
-                            print(python_command)
-                            uid += 1
+                                    name += "_Count_Cts_{}_Beta_{}_uid_{}".format(cts_size, beta, uid)
+                                else:
+                                    name += "_DQN_uid_{}".format(uid)
+                                python_command = "python3 RL_Trainer_PyTorch.py --name {} --env {} --lr {} --seed {} --t-max {} --eps-start {}".format(name, env, lr, seed, t_max, eps)
+                                if count:
+                                    python_command += " --count --beta {} --cts-size {}".format(beta, cts_size)
+                                if gpu:
+                                    python_command += " --gpu"
+                                if debug_eval:
+                                    python_command += " --debug-eval"
+                                if screen:
+                                    screen_name = "DQN"
+                                    if count:
+                                        screen_name = "Count"
+                                    screen_name += "_{}".format(seed)
+                                    python_command = "screen -mdS {}__{} bash -c \"{}\"".format(uid, screen_name, python_command)
+                                print(python_command)
+                                uid += 1
