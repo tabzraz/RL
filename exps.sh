eb8a098027abb94a552e9f2a106e27139b42528f
Tabz <tabzraz@gmail.com>
2017-03-15 19:31:33 +0000

    .

--- exps.sh
+++ exps.sh
@@ -1,4 +1,4 @@
-NV_GPU="0" screen -mdS Exps1 bash -c "bash exps1.sh"
-NV_GPU="1" screen -mdS Exps2 bash -c "bash exps2.sh"
-NV_GPU="2" screen -mdS Exps3 bash -c "bash exps3.sh"
-NV_GPU="3" screen -mdS Exps4 bash -c "bash exps4.sh"
+screen -mdS Exps1 bash -c "NV_GPU='0' bash exps1.sh"
+screen -mdS Exps2 bash -c "NV_GPU='1' bash exps2.sh"
+screen -mdS Exps3 bash -c "NV_GPU='2' bash exps3.sh"
+screen -mdS Exps4 bash -c "NV_GPU='3' bash exps4.sh"
