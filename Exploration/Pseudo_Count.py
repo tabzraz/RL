b33f6dadd01f45b2d71b19b934763af235d53a0f
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-05-01 16:27:36 +0100

    Increase max pseudo-count

--- Exploration/Pseudo_Count.py
+++ Exploration/Pseudo_Count.py
@@ -48,7 +48,7 @@ class PseudoCount:
 
         extra_info["Pixel_PG"] = pg_pixel
 
-        pg = min(10, pg)
+        pg = min(100, pg)
         pg = max(0.001, pg)
         pseudo_count = 1 / (np.expm1(pg))
         extra_info["Pseudo_Count"] = pseudo_count
