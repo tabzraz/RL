e0c33bf72d5133a063aa3e373b6ebe9aaa7d9aac
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-05-02 18:16:00 +0100

    Count state action experiments

--- Exp_Gen.py
+++ Exp_Gen.py
@@ -1,6 +1,6 @@
 envs = ["Med-Maze-{}-v0".format(size) for size in [10]]
 lrs = [0.0001]
-counts = [True, False]
+counts = [True]
 cts_convs = [False]
 betas = [0.0001]
 t_maxs = [300000]
@@ -11,14 +11,12 @@ epsilon_steps = [200000]
 batch_sizes = [32]
 xp_replay_sizes = [30000]
 stale_limits = [1]
-epsilon_scaling = [False, True]
-n_steps = [1, 5, 10]
+epsilon_scaling = [False]
+n_steps = [1]
+
+state_action_modes = ["Plain", "Force", "Optimistic"]
 
 options = [False]
-
-num_macros = [1]#[10, 50, 200]
-max_macro_lengths = [1]#[8, 16, 32]
-macro_seeds = [1]#[5, 6]
 
 gpu = True
 # debug_eval = True
@@ -40,61 +38,78 @@ for env in envs:
                     for lr in lrs:
                         for eps, eps_steps in zip(epsilon_starts, epsilon_steps):
                             for count, eps_scaling in [(c, e) for c in counts for e in epsilon_scaling if not (c is False and e is True)]:
-                                for stale_val in stale_limits:
-                                    for beta in betas:
-                                        for cts_size in cts_sizes:
-                                            for cts_conv in cts_convs:
-                                                for seed in seeds:
-                                                    name = env.replace("-", "_")[:-3]
-                                                    name += "_{}_Step".format(n_step)
-                                                    name += "_LR_{}".format(lr)
-                                                    name += "_Batch_{}_XpSize_{}k".format(batch_size, str(xp_replay_size)[:-3])
-                                                    # if option:
-                                                    #     if random_macros:
-                                                    #         name += "_Rnd_Macros_{}_Length_{}_Mseed_{}_Primitives_{}".format(num_macro, max_macro_length, macro_seed, with_primitives)
-                                                    #     else:
-                                                    #         name += "_Options"
-                                                    if count:
-                                                        stale = int(xp_replay_size * stale_val)
-                                                        # print(stale)
-                                                        if eps_scaling:
-                                                            name += "_CountEps"
-                                                        name += "_Count_Cts_{}_Stale_{}k_Conv_{}_Beta_{}_Eps_{}_uid_{}".format(cts_size, str(stale)[:-3], cts_conv, beta, eps, uid)
-                                                    else:
-                                                        name += "_DQN_Eps_{}_uid_{}".format(eps, uid)
-                                                    python_command = "python3 Main.py --name {} --env {} --lr {} --seed {} --t-max {} --eps-start {} --batch-size {} --xp {}".format(name, env, lr, seed, t_max, eps, batch_size, xp_replay_size)
-                                                    python_command += " --eps-steps {}".format(eps_steps)
-                                                    python_command += " --n-step {}".format(n_step)
-                                                    if count:
-                                                        python_command += " --count --beta {} --cts-size {}".format(beta, cts_size)
-                                                        python_command += " --stale-limit {}".format(stale)
-                                                        if eps_scaling:
-                                                            python_command += " --count-epsilon"
-                                                        if cts_conv:
-                                                            python_command += " --cts-conv"
-                                                    # if option:
-                                                    #     if random_macros:
-                                                    #         python_command += " --options Random_Macros --num-macros {} --max-macro-length {} --macro-seed {}".format(num_macro, max_macro_length, macro_seed)
-                                                    #         if with_primitives:
-                                                    #             python_command += " --train-primitives"
-                                                    #     else:
-                                                    #         python_command += " --options Maze_Good"
-                                                    if gpu:
-                                                        python_command += " --gpu"
-                                                    # if debug_eval:
-                                                        # python_command += " --debug-eval"
-                                                    if tar:
-                                                        python_command += " --tar"
-                                                    if screen:
-                                                        screen_name = "DQN"
+                                for state_action_mode in state_action_modes:
+                                    for stale_val in stale_limits:
+                                        for beta in betas:
+                                            for cts_size in cts_sizes:
+                                                for cts_conv in cts_convs:
+                                                    for seed in seeds:
+
+                                                        if state_action_mode != None and count is False:
+                                                            continue
+
+                                                        name = env.replace("-", "_")[:-3]
+                                                        name += "_{}_Step".format(n_step)
+                                                        name += "_LR_{}".format(lr)
+                                                        name += "_Batch_{}_XpSize_{}k".format(batch_size, str(xp_replay_size)[:-3])
+                                                        # if option:
+                                                        #     if random_macros:
+                                                        #         name += "_Rnd_Macros_{}_Length_{}_Mseed_{}_Primitives_{}".format(num_macro, max_macro_length, macro_seed, with_primitives)
+                                                        #     else:
+                                                        #         name += "_Options"
                                                         if count:
-                                                            screen_name = "Count"
-                                                        screen_name += "_{}".format(seed)
-                                                        python_command = "screen -mdS {}__{} bash -c \"{}\"".format(uid, screen_name, python_command)
-                                                    print(python_command)
-                                                    commands.append(python_command)
-                                                    uid += 1
-                                                print()
+                                                            stale = int(xp_replay_size * stale_val)
+                                                            # print(stale)
+                                                            if eps_scaling:
+                                                                name += "_CountEps"
+                                                            if state_action_mode == "Plain":
+                                                                name += "_StateAction"
+                                                            elif state_action_mode == "Force":
+                                                                name += "_ForceAction"
+                                                            elif state_action_mode == "Optimistic":
+                                                                name += "_OptimisticAction"
+                                                            name += "_Count_Cts_{}_Stale_{}k_Conv_{}_Beta_{}_Eps_{}_uid_{}".format(cts_size, str(stale)[:-3], cts_conv, beta, eps, uid)
+                                                        else:
+                                                            name += "_DQN_Eps_{}_uid_{}".format(eps, uid)
+                                                        python_command = "python3 Main.py --name {} --env {} --lr {} --seed {} --t-max {} --eps-start {} --batch-size {} --xp {}".format(name, env, lr, seed, t_max, eps, batch_size, xp_replay_size)
+                                                        python_command += " --eps-steps {}".format(eps_steps)
+                                                        python_command += " --n-step {}".format(n_step)
+                                                        if count:
+                                                            python_command += " --count --beta {} --cts-size {}".format(beta, cts_size)
+                                                            python_command += " --stale-limit {}".format(stale)
+                                                            if eps_scaling:
+                                                                python_command += " --count-epsilon"
+                                                            if cts_conv:
+                                                                python_command += " --cts-conv"
+                                                            if state_action_mode == "Plain":
+                                                                python_command += " --count-state-action"
+                                                            elif state_action_mode == "Force":
+                                                                python_command += " --force-low-count-action"
+                                                            elif state_action_mode == "Optimistic":
+                                                                python_command += " --optimistic-init"
+                                                        # if option:
+                                                        #     if random_macros:
+                                                        #         python_command += " --options Random_Macros --num-macros {} --max-macro-length {} --macro-seed {}".format(num_macro, max_macro_length, macro_seed)
+                                                        #         if with_primitives:
+                                                        #             python_command += " --train-primitives"
+                                                        #     else:
+                                                        #         python_command += " --options Maze_Good"
+                                                        if gpu:
+                                                            python_command += " --gpu"
+                                                        # if debug_eval:
+                                                            # python_command += " --debug-eval"
+                                                        if tar:
+                                                            python_command += " --tar"
+                                                        if screen:
+                                                            screen_name = "DQN"
+                                                            if count:
+                                                                screen_name = "Count"
+                                                            screen_name += "_{}".format(seed)
+                                                            python_command = "screen -mdS {}__{} bash -c \"{}\"".format(uid, screen_name, python_command)
+                                                        print(python_command)
+                                                        commands.append(python_command)
+                                                        uid += 1
+                                                    print()
 
 if write_to_files:
     for i in range(files):
