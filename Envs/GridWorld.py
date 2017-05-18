3d1da49c412f98340108634dff8575774248ceab
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-05-18 17:50:10 +0100

    Optimistic bandit scaling expriments

--- Envs/GridWorld.py
+++ Envs/GridWorld.py
@@ -215,7 +215,7 @@ class GridWorld(gym.Env):
 
             if np.max(canvas) == 0:
                 break
-            canvas = canvas / max_bonus
+            canvas = canvas / np.max(canvas)
 
             # TODO: Colour the unvisited goals
             for goal in self.goals_order:
