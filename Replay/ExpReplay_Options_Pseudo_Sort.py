dabe1f2abfa468bc2a23be7754a305ee98c84743
Tabz <tabzraz@gmail.com>
2017-06-05 23:27:32 +0100

    fixing n step q learning for count sorted replay

--- Replay/ExpReplay_Options_Pseudo_Sort.py
+++ Replay/ExpReplay_Options_Pseudo_Sort.py
@@ -17,6 +17,8 @@ class ExperienceReplay_Options_Pseudo_Sort:
         self.experiences_stored = 0
         self.args = args
 
+        self.recent_exps = []
+
         self.priority = priority
         if self.priority:
             self.alpha = 0.7
@@ -34,12 +36,29 @@ class ExperienceReplay_Options_Pseudo_Sort:
             # self.Exps.pop(0)
         # Make copies just in case
         new_exp = self.Experience(state=np.copy(state_now), action=action, reward=reward, state_next=np.copy(state_after), steps=steps, terminal=terminal, pseudo_reward=pseudo_reward, pseudo_reward_t=self.T, trajectory_end=terminal)
-        self.Exps[self.storing_index] = new_exp
+        self.recent_exps.append(new_exp)
+        if len(self.recent_exps) < self.args.n_step:
+            return
+        n_step_reward = 0
+        n_step_psuedo_reward = 0
+        for i in range(self.args.n_step):
+            n_step_reward += (self.args.gamma ** i) * self.recent_exps[i].reward
+            n_step_psuedo_reward += (self.args.gamma ** i) * self.recent_exps[i].pseudo_reward
+        n_step_state = np.copy(self.recent_exps[0].state)
+        n_step_action = self.recent_exps[0].action
+        n_step_terminal = self.recent_exps[-1].terminal
+        n_step_traj_end = self.recent_exps[-1].trajectory_end
+        n_step_next_state = np.copy(self.recent_exps[-1].state_next)
+        n_step_exp = self.Experience(state=n_step_state, action=n_step_action, reward=n_step_reward, state_next=n_step_next_state, steps=self.args.n_step, terminal=n_step_terminal, pseudo_reward=n_step_psuedo_reward, pseudo_reward_t=self.T, trajectory_end=n_step_traj_end)
+
+        self.Exps[self.storing_index] = n_step_exp
+        # Remove the oldest entry
+        self.recent_exps = self.recent_exps[1:]
 
         if self.priority:
             p = self.priorities.get_max_priority()
             self.priorities.update(p, self.storing_index)
-            if self.storing_index % 1000 == 0:
+            if self.T % 1000 == 0:
                 self.priorities.balance_tree()
 
         self.experiences_stored = max(self.experiences_stored, self.storing_index)
@@ -53,11 +72,44 @@ class ExperienceReplay_Options_Pseudo_Sort:
             self.storing_index += 1
 
     def end_of_trajectory(self):
-        exp = self.Exps[self.storing_index - 1]
-        new_exp = list(exp)
-        new_exp[-1] = True  # End of trajectory
-        new_exp_tuple = self.Experience(*new_exp)
-        self.Exps[self.storing_index - 1] = new_exp_tuple
+        while len(self.recent_exps) > 0:
+            n_step_reward = 0
+            n_step_psuedo_reward = 0
+            for i in range(len(self.recent_exps)):
+                n_step_reward += (self.args.gamma ** i) * self.recent_exps[i].reward
+                n_step_psuedo_reward += (self.args.gamma ** i) * self.recent_exps[i].pseudo_reward
+            n_step_state = np.copy(self.recent_exps[0].state)
+            n_step_action = self.recent_exps[0].action
+            n_step_terminal = self.recent_exps[-1].terminal
+            # n_step_traj_end = self.recent_exps[-1].trajectory_end
+            n_step_next_state = np.copy(self.recent_exps[-1].state_next)
+            n_step_exp = self.Experience(state=n_step_state, action=n_step_action, reward=n_step_reward, state_next=n_step_next_state, steps=len(self.recent_exps), terminal=n_step_terminal, pseudo_reward=n_step_psuedo_reward, pseudo_reward_t=self.T, trajectory_end=True)
+
+            self.Exps[self.storing_index] = n_step_exp
+            # Remove the oldest entry
+            self.recent_exps = self.recent_exps[1:]
+
+            if self.priority:
+                p = self.priorities.get_max_priority()
+                self.priorities.update(p, self.storing_index)
+                if self.T % 1000 == 0:
+                    self.priorities.balance_tree()
+
+            self.experiences_stored = max(self.experiences_stored, self.storing_index)
+
+            if self.storing_index == self.N - 1:
+                # We have stored the most experiences we can
+                # Sort the list by count (pseudo-reward as proxy) now
+                self.Exps.sort(key=lambda x: x.pseudo_reward, reverse=True)
+
+            if self.storing_index < self.N - 1:
+                self.storing_index += 1
+
+        # exp = self.Exps[self.storing_index - 1]
+        # new_exp = list(exp)
+        # new_exp[-1] = True  # End of trajectory
+        # new_exp_tuple = self.Experience(*new_exp)
+        # self.Exps[self.storing_index - 1] = new_exp_tuple
 
     def get_sampling_distribution(self):
         if self.experiences_stored < self.N:
@@ -97,47 +149,6 @@ class ExperienceReplay_Options_Pseudo_Sort:
                 # print(index, priority)
                 self.priorities.update(priority[0] + 0.00001, index)
 
-    def Sample(self, size):
-        # assert(size <= self.N)
-        # if len(self.Exps) < size:
-            # return self.Exps
-        # indices = np.random.randint(low=0, high=len(self.Exps) - 1, size=size)
-        self.T += 1
-        indices = self.get_indices(size)
-        self.Recompute_Pseudo_Counts(indices)
-        # sample = [self.Exps[i] for i in indices]
-        return [self.Exps[i] for i in indices], indices
-
-    def Sample_Sequence(self, size, seq_length):
-        # indices = np.random.randint(low=0, high=len(self.Exps) - 1, size=size)
-        self.T += 1
-        indices = self.get_indices(size)
-        states = []
-        actions = []
-        rewards = []
-        next_states = []
-        terminals = []
-        for index in indices:
-            exps_to_use = self.Exps[index: index + seq_length]
-            # Check for terminal states
-            index_up_to = seq_length
-            for i, exp in enumerate(exps_to_use):
-                if exp[5]:
-                    index_up_to = i + 1
-                    break
-            exps_to_use = exps_to_use[:index_up_to]
-
-            states.append([x[0] for x in exps_to_use])
-            actions.append([x[1] for x in exps_to_use])
-            rewards.append([x[2] for x in exps_to_use])
-            next_states.append([x[3] for x in exps_to_use])
-            terminals.append([x[5] for x in exps_to_use])
-
-        return states, actions, rewards, next_states, terminals
-
-
-    # Sample an n-step target
-    # Intended for use with step sizes of 1 for now
     def Sample_N(self, size, N, gamma):
         assert(size <= self.N)
         self.T += 1
@@ -145,29 +156,26 @@ class ExperienceReplay_Options_Pseudo_Sort:
         indices = self.get_indices(size)
         self.Recompute_Pseudo_Counts(indices)
         batch_to_return = []
-        for index in indices:
-            exps_to_use = self.Exps[index: min(self.experiences_stored, index + N)]
-            # Check for terminal states
-            index_up_to = min(self.experiences_stored, index + N) - index
-            for i, exp in enumerate(exps_to_use):
-                # print(exp)
-                if exp.terminal or exp.trajectory_end:
-                    index_up_to = i + 1
-                    break
-            exps_to_use = exps_to_use[:index_up_to]
-            # We then need to recompute the pseudo-counts for all of these
-            # print([ii for ii in range(index, index + index_up_to)])
-            self.Recompute_Pseudo_Counts([ii for ii in range(index, index + index_up_to)])
 
-            state_now = exps_to_use[0].state
-            action_now = exps_to_use[0].action
-            state_then = exps_to_use[-1].state_next
-            terminate = exps_to_use[-1].terminal
-            steps = len(exps_to_use)
-            rewards_to_use = list(map(lambda x: x.reward + x.pseudo_reward, exps_to_use))
-            accum_reward = 0
-            for ri in reversed(rewards_to_use):
-                accum_reward = ri + gamma * accum_reward
+        exps_to_use = self.Exps[indices]
+
+        for exp in exps_to_use:
+            state_now = exp.state
+            action_now = exp.action
+            accum_reward = exp.reward + exp.pseudo_reward
+            state_then = exp.state_next
+            steps = exp.steps
+            terminate = exp.terminal
+
+        # state_now = exps_to_use[0].state
+        # action_now = exps_to_use[0].action
+        # state_then = exps_to_use[-1].state_next
+        # terminate = exps_to_use[-1].terminal
+        # steps = len(exps_to_use)
+        # rewards_to_use = list(map(lambda x: x.reward + x.pseudo_reward, exps_to_use))
+        # accum_reward = 0
+        # for ri in reversed(rewards_to_use):
+        #     accum_reward = ri + gamma * accum_reward
             new_exp = (state_now, action_now, accum_reward, state_then, steps, terminate)
             batch_to_return.append(new_exp)
         # [] for indices to match the prioritized replay
