7a08bf27db355e834dfbdd11057432e8a8ad4e68
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-07-04 12:32:57 +0100

    Prioritised with 0.5 alpha scaling

--- Main.py
+++ Main.py
@@ -88,7 +88,7 @@ parser.add_argument("--count-td-scaler", type=float, default=1)
 parser.add_argument("--variable-n-step", action="store_true", default=False)
 parser.add_argument("--negative-td-scaler", type=float, default=1)
 parser.add_argument("--density-priority", action="store_true", default=False)
-parser.add_argument("--alpha", type=float, default=0.7)
+parser.add_argument("--alpha", type=float, default=0.5)
 args = parser.parse_args()
 
 if args.force_low_count_action or args.optimistic_init:
