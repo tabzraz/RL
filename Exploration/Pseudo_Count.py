ebf3ab4f5cd4d3e9e5b4f63349f1b42f4aeb1434
Tabz <tabzraz@gmail.com>
2017-07-13 03:05:41 +0100

    Reward clipping and scaling fix

--- Exploration/Pseudo_Count.py
+++ Exploration/Pseudo_Count.py
@@ -64,11 +64,11 @@ class PseudoCount:
         extra_info["Pseudo_Count"] = pseudo_count
 
         bonus = self.beta / sqrt(pseudo_count + 0.01)
-        if self.args.negative_rewards:
-            bonus -= self.beta * self.args.negative_reward_threshold
 
         if bonus < self.beta * self.args.bonus_clip:
             bonus = 0
+        if self.args.negative_rewards:
+            bonus -= self.beta * self.args.negative_reward_threshold
 
         extra_info["Bonus"] = bonus
 
