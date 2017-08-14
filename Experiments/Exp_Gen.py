917a37054aca18e4374630c5688db335e58a441b
Tabz <tabzraz@gmail.com>
2017-08-14 17:18:45 +0100

    ThinMaze14 4conv dueling network exps

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -9,12 +9,12 @@ counts = [True]
 betas = [0.0001]
 t_maxs = [x * 1000 for x in [1200]]
 cts_sizes = [21]
-num_seeds = 8
+num_seeds = 4
 epsilon_starts = [0.05]
 epsilon_finishs = [0.05]
 epsilon_steps = [50000]
 batch_sizes = [(32, 1)]
-xp_replay_sizes = [x * 1000 for x in [300, 500]]
+xp_replay_sizes = [x * 1000 for x in [100, 300]]
 stale_limits = [x * 1000 for x in [1000]]
 epsilon_scaling = [True]
 epsilon_decay = [0.9999]
@@ -57,7 +57,7 @@ if False in prioritiseds:
 count_td_scalers = [1]
 density_priority = False
 eligibility_trace = False
-gammas = [0.9999]
+gammas = [0.9999, 0.99]
 
 set_replays = [(False, 1)]
 doubles = [False]
