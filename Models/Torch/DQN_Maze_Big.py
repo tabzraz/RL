140328dfd2fa9e44f795ed4accb2f80335ca9bc3
Tabz <tabzraz@gmail.com>
2017-08-14 17:16:51 +0100

    BigMaze Model now has 4 conv layers, 1 padding and dueling

--- Models/Torch/DQN_Maze_Big.py
+++ Models/Torch/DQN_Maze_Big.py
@@ -7,26 +7,43 @@ class DQN(nn.Module):
     def __init__(self, input_size=(1, 60, 60), actions=1):
         super(DQN, self).__init__()
 
+        self.actions = actions
+
         img_size = input_size[1]
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=0, stride=2)
-        img_size = int((img_size - 3) / 2 + 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=0, stride=2)
-        img_size = int((img_size - 3) / 2 + 1)
-        self.conv3 = nn.Conv2d(64, 64, 3, padding=0, stride=2)
-        img_size = int((img_size - 3) / 2 + 1)
-        self.fc1 = nn.Linear(img_size * img_size * 64, 128)
-        self.fc2 = nn.Linear(128, actions)
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.fc1 = nn.Linear(img_size * img_size * 32, 128)
+        self.value_branch = nn.Linear(128, 1)
+        self.adv_branch = nn.Linear(128, actions)
 
     def forward(self, x):
         if next(self.parameters()).is_cuda:
             x = x.cuda()
-
+        # Conv layers
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
-
+        x = F.relu(self.conv4(x))
+        # Flatten the final conv layer
         x = x.view(x.size()[0], -1)
 
+        # FC layer
         x = F.relu(self.fc1(x))
 
-        return self.fc2(x)
+        # Value branch
+        v = self.value_branch(x).expand(x.size(0), self.actions)
+
+        # Advantages branch
+        advs = self.adv_branch(x)
+        advs_mean = advs.mean(1).expand(x.size(0), self.actions)
+
+        # Dueling output
+        outputs = v + (advs - advs_mean)
+
+        return outputs
