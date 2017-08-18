c1155980426920568770b264480e6ec4e1e3819d
Tabz <tabzraz@gmail.com>
2017-08-18 18:11:31 +0100

    4 layer 3x3 conv 2 stride 1 padding no dueling

--- Models/Torch/DQN_Maze_Big.py
+++ Models/Torch/DQN_Maze_Big.py
@@ -10,17 +10,18 @@ class DQN(nn.Module):
         self.actions = actions
 
         img_size = input_size[1]
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=0, stride=1)
-        img_size = int((img_size - 3 + 2 * 0) / 1 + 1)
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
         self.conv2 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
         img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
         self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
         img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
         self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
         img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
-        self.fc1 = nn.Linear(img_size * img_size * 32, 256)
-        self.value_branch = nn.Linear(256, 1)
-        self.adv_branch = nn.Linear(256, actions)
+        self.fc1 = nn.Linear(img_size * img_size * 32, 128)
+        self.qvals = nn.Linear(128, actions)
+        # self.value_branch = nn.Linear(128, 1)
+        # self.adv_branch = nn.Linear(128, actions)
 
     def forward(self, x):
         if next(self.parameters()).is_cuda:
@@ -36,14 +37,18 @@ class DQN(nn.Module):
         # FC layer
         x = F.relu(self.fc1(x))
 
+        x = self.qvals(x)
+
+        return x
+
         # Value branch
-        v = self.value_branch(x).expand(x.size(0), self.actions)
+        # v = self.value_branch(x).expand(x.size(0), self.actions)
 
         # Advantages branch
-        advs = self.adv_branch(x)
-        advs_mean = advs.mean(1).expand(x.size(0), self.actions)
+        # advs = self.adv_branch(x)
+        # advs_mean = advs.mean(1).expand(x.size(0), self.actions)
 
         # Dueling output
-        outputs = v + (advs - advs_mean)
+        # outputs = v + (advs - advs_mean)
 
-        return outputs
+        # return outputs
