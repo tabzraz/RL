7811fc71812afa3727e9ac2538c1619d7488bb6e
Tabz <tabzraz@gmail.com>
2017-08-28 16:41:57 +0100

    Prioritising with pcdecay

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -45,7 +45,7 @@ files = 16
 # prioritizeds = [(True, False, 8, False, 0.5)]  # [(True, False, True), (True, True, True)]
 
 alphas = [0.5]
-prioritiseds = [False]
+prioritiseds = [True]
 is_corrections = [False]
 minus_pseudos = [False]
 negative_td_scalers = [1]
