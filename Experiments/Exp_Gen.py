4d2ec9ebc8e352de22251f66d7c2d0faf8b793f5
Tabz <tabzraz@gmail.com>
2017-09-05 15:50:43 +0100

    ThinMaze18 mark2 xp replay experiments

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -1,7 +1,7 @@
 import sys
 from math import ceil
 
-envs = ["Thin-Maze-{}-v0".format(size) for size in [14]]
+envs = ["Thin-Maze-{}-v0".format(size) for size in [18]]
 target_network = 1000
 lrs = [0.0001]
 counts = [True]
@@ -9,14 +9,14 @@ counts = [True]
 betas = [0.0001]
 t_maxs = [x * 1000 for x in [1200]]
 cts_sizes = [21]
-num_seeds = 4
-epsilon_starts = [0.05, 0.25, 0.5, 0.75, 1, 1, 1, 1] #[0.05]
-epsilon_finishs = [0.05, 0.25, 0.5, 0.75, 0.05, 0.05, 0.05, 0.05]
-epsilon_steps = [50000, 50000, 50000, 50000] + [x * 1000 for x in [50, 300, 600, 900]]
+num_seeds = 8
+epsilon_starts = [0.05] #[0.05]
+epsilon_finishs = [0.05]
+epsilon_steps = [50000]
 batch_sizes = [(32, 1)]
-xp_replay_sizes = [x * 1000 for x in [100]]
+xp_replay_sizes = [x * 1000 for x in [100, 300, 500, 700]]
 stale_limits = [x * 1000 for x in [1000]]
-epsilon_scaling = [False] #[True]
+epsilon_scaling = [True]
 epsilon_decay = [0.9999]
 
 n_steps = [100]
