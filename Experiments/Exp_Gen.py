69246e0ef4ae841e6645a91f545bb6007b646822
Tabz <tabzraz@gmail.com>
2017-09-20 14:07:39 +0100

    ThinMaze14  Bandit and UCB hyperparams

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -1,20 +1,20 @@
 import sys
 from math import ceil
 
-envs = ["Thin-Maze-{}-v0".format(size) for size in [20]]
+envs = ["Thin-Maze-{}-v0".format(size) for size in [14]]
 target_network = 1000
 lrs = [0.0001]
 counts = [True]
 # cts_convs = [False]
 betas = [0.0001]
-t_maxs = [x * 1000 for x in [3000]]
+t_maxs = [x * 1000 for x in [1200]]
 cts_sizes = [21]
-num_seeds = 8
+num_seeds = 4
 epsilon_starts = [0.05]
 epsilon_finishs = [0.05]
 epsilon_steps = [1]
 batch_sizes = [(32, 1)]
-xp_replay_sizes = [x * 1000 for x in [100, 300, 500, 700, 900]]
+xp_replay_sizes = [x * 1000 for x in [100]]
 stale_limits = [x * 1000 for x in [1000]]
 epsilon_scaling = [True]
 epsilon_decay = [0.9999]
@@ -28,11 +28,11 @@ reward_clips = [-1]
 
 # state_action_modes = ["Plain", "Force", "Optimistic"]
 # state_action_modes = [None]
-optimism_scalers = [0]
-state_action_modes = [None]# + ["Optimistic" for _ in range(1)]
-force_scalers = [0] 
+optimism_scalers = [1, 10, 0.00001, 1, 10, 0.0001]
+state_action_modes = ["Optimistic" for _ in optimism_scalers]
+force_scalers = [0 for _ in optimism_scalers]
 bandit_no_epsilon_scaling = True #HACK
-ucb_bandits = [False]
+ucb_bandits = [True, True, True, False, False, False]
 
 n_step_mixings = [1.0]
 
