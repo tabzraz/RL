9c88a717bc9830f77be0de130866f5243438f93b
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-10-03 17:09:51 +0100

    NEC model change

--- Models/Torch/NEC_Maze.py
+++ Models/Torch/NEC_Maze.py
@@ -10,12 +10,12 @@ class NEC_Embedding(nn.Module):
         img_size = input_size[1]
         self.conv1 = nn.Conv2d(1, 32, 3, padding=1, stride=2)
         img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
-        # self.conv2 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
-        # img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
-        # self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
-        # img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
-        # self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
-        # img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv3 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
+        self.conv4 = nn.Conv2d(32, 32, 3, padding=1, stride=2)
+        img_size = int((img_size - 3 + 2 * 1) / 2 + 1)
         self.fc1 = nn.Linear(img_size * img_size * 32, embedding)
         # self.value_branch = nn.Linear(128, 1)
         # self.adv_branch = nn.Linear(128, actions)
@@ -25,9 +25,9 @@ class NEC_Embedding(nn.Module):
             x = x.cuda()
         # Conv layers
         x = F.relu(self.conv1(x))
-        # x = F.relu(self.conv2(x))
-        # x = F.relu(self.conv3(x))
-        # x = F.relu(self.conv4(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
         # Flatten the final conv layer
         x = x.view(x.size()[0], -1)
 
