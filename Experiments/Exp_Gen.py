b1efff380648d4c1d4e537211026e0938a377449
Tabz <tabzraz@gmail.com>
2017-10-08 18:15:51 +0100

    ThinMaze12 xp sizes experiments

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -1,25 +1,25 @@
 import sys
 from math import ceil
 
-envs = ["Thin-Maze-{}-Neg-v0".format(size) for size in [10]]
+envs = ["Thin-Maze-{}-Neg-v0".format(size) for size in [12]]
 target_network = 1000
 lrs = [0.0001] # 0.0001
 counts = [True]
 # cts_convs = [False]
-betas = [0.01, 0.001] # 0.0001
-t_maxs = [x * 1000 for x in [600]]
+betas = [0.1, 0.01, 0.001] # 0.0001
+t_maxs = [x * 1000 for x in [1000]]
 cts_sizes = [12]
 num_seeds = 4
 epsilon_starts = [0.05]
 epsilon_finishs = [0.05]
 epsilon_steps = [1]
 batch_sizes = [(32, 1)]
-xp_replay_sizes = [x * 1000 for x in [25, 100]]
-stale_limits = [x * 1000 for x in [100]]
+xp_replay_sizes = [x * 1000 for x in [50, 100, 200]]
+stale_limits = [x * 1000 for x in [300]]
 epsilon_scaling = [True]
 epsilon_decay = [0.9999]
 
-n_steps = [1, 10]
+n_steps = [1, 10, 100]
 variable_n_step = False
 
 negative_rewards = [(False, 0)]
@@ -34,7 +34,7 @@ force_scalers = [0 for _ in optimism_scalers]
 bandit_no_epsilon_scaling = True #HACK
 ucb_bandits = [False] #[True, True, True, False, False, False]
 
-bonus_replay = True
+bonus_replay = False
 bonus_replay_thresholds = [0.5, 0.75, 0.9]
 if not bonus_replay:
     bonus_replay_thresholds = [1]
