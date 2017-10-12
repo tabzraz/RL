45e397a9877966490c1a8d71836760031bfa6790
Tabish Rashid <tabish.rashid@cs.ox.ac.uk>
2017-10-12 14:49:49 +0100

    DQN Distrib agent seems to work

--- Agent/DQN_Distribution_Agent.py
+++ Agent/DQN_Distribution_Agent.py
@@ -98,7 +98,7 @@ class DQN_Distribution_Agent:
         if np.random.random() < epsilon:
             action = np.random.randint(low=0, high=self.args.actions)
         else:
-            action = np.argmax(q_values_numpy)
+            action = int(np.argmax(q_values_numpy))
             # action = q_values.max(0)[1][0]  # Torch...
 
         extra_info["Action"] = action
@@ -139,59 +139,92 @@ class DQN_Distribution_Agent:
             target_dqn_qvals = self.target_dqn(new_states).cpu()
             # Make a new variable with those values so that these are treated as constants
             target_dqn_qvals_data = Variable(target_dqn_qvals.data)
-            q_value_targets = (Variable(torch.ones(terminal_states.size()[0])) - terminal_states)
+
+            q_value_gammas = (Variable(torch.ones(terminal_states.size()[0])) - terminal_states)
             inter = Variable(torch.ones(terminal_states.size()[0]) * self.args.gamma)
             # print(steps)
-            q_value_targets = q_value_targets * torch.pow(inter, steps)
+            q_value_gammas = q_value_gammas * torch.pow(inter, steps)
 
-            # print(q_value_targets)
-            q_value_targets = q_value_targets.view(self.args.batch_size, 1, 1)
-            q_value_targets = q_value_targets.expand(self.args.batch_size, self.args.actions, self.args.atoms)
-            # q_value_targets = q_value_targets.repeat(self.args.batch_size, self.args.actions, 1)
-            # print(q_value_targets, target_dqn_qvals)
-            q_value_targets = q_value_targets * target_dqn_qvals_data
-            # print(q_value_targets)
+            values = torch.linspace(self.args.v_min, self.args.v_max, steps=self.args.atoms)
+            values = Variable(values)
+            values = values.view(1, 1, self.args.atoms)
+            values = values.expand(self.args.batch_size, self.args.actions, self.args.atoms)
+            # print(values)
 
-            # print(q_value_targets, rewards)
+            q_value_gammas = q_value_gammas.view(self.args.batch_size, 1, 1)
+            q_value_gammas = q_value_gammas.expand(self.args.batch_size, self.args.actions, self.args.atoms)
+            # print(q_value_gammas)
+            gamma_values = q_value_gammas * values
+            # print(gamma_values)
             rewards = rewards.view(self.args.batch_size, 1, 1)
             rewards = rewards.expand(self.args.batch_size, self.args.actions, self.args.atoms)
-            q_value_targets = q_value_targets + rewards
-            # print(q_value_targets)
+            # print(rewards)
+            operator_q_values = rewards + gamma_values
+            # print(operator_q_values)
 
-            # Have to project the q_value_targets back onto the original support
+            clipped_operator_q_values = torch.clamp(operator_q_values, self.args.v_min, self.args.v_max)
 
-            # AM DOING THIS ALL WRONG, WE SHOULD BE USING THE ATOMS NOT THE PROBABILITIES
+            delta_z = (self.args.v_max - self.args.v_min) / (self.args.atoms - 1)
+            # Using the notation from the categorical paper
+            b_j = (clipped_operator_q_values - self.args.v_min) / delta_z
+            # print(b_j)
+            lower_bounds = torch.floor(b_j)
+            upper_bounds = torch.ceil(b_j)
+
+            # Work out the max action
+            atom_values = Variable(torch.linspace(self.args.v_min, self.args.v_max, steps=self.args.atoms))
+            atom_values = atom_values.view(1, 1, self.args.atoms)
+            atom_values = atom_values.expand(self.args.batch_size, self.args.actions, self.args.atoms)
+
+            # Sum over the atoms dimension
+            target_expected_qvalues = torch.sum(target_dqn_qvals_data * atom_values, dim=2)
+            # Get the maximum actions index across the batch size
+            max_actions = target_expected_qvalues.max(dim=1)[1].view(-1)
+
+            # Project back onto the original support for the max actions
+            q_value_distribution_targets = torch.zeros(self.args.batch_size, self.args.atoms)
+
+            # Distributions for the max actions
+            # print(target_dqn_qvals_data, max_actions)
+            q_value_max_actions_distribs = target_dqn_qvals_data.index_select(dim=1, index=max_actions)[:,0,:]
+            # print(q_value_max_actions_distribs)
+
+            # Lower_bounds_actions
+            lower_bounds_actions = lower_bounds.index_select(dim=1, index=max_actions)[:,0,:]
+            upper_bounds_actions = upper_bounds.index_select(dim=1, index=max_actions)[:,0,:]
+            b_j_actions = b_j.index_select(dim=1, index=max_actions)[:,0,:]
+
+            lower_bound_values_to_add = q_value_max_actions_distribs * (upper_bounds_actions - b_j_actions)
+            upper_bound_values_to_add = q_value_max_actions_distribs * (b_j_actions - lower_bounds_actions)
+            # print(lower_bounds_actions)
+            # print(lower_bound_values_to_add)
+            # Naive looping
+            for b in range(self.args.batch_size):
+                for l, pj in zip(lower_bounds_actions.data.type(torch.LongTensor)[b], lower_bound_values_to_add[b].data):
+                    q_value_distribution_targets[b][l] += pj
+                for u, pj in zip(upper_bounds_actions.data.type(torch.LongTensor)[b], upper_bound_values_to_add[b].data):
+                    q_value_distribution_targets[b][u] += pj
 
             self.dqn.train()
             if self.args.gpu:
                 actions = actions.cuda()
                 q_value_targets = q_value_targets.cuda()
-            model_predictions = self.dqn(states).gather(1, actions.view(-1, 1))
+            model_predictions = self.dqn(states).index_select(1, actions.view(-1))[:,0,:]
+            q_value_distribution_targets = Variable(q_value_distribution_targets)
+            # print(q_value_distribution_targets)
+            # print(model_predictions) 
 
-            # info = {}
+            # Cross entropy loss
+            ce_loss = -torch.sum(q_value_distribution_targets * torch.log(model_predictions), dim=1)
+            ce_batch_loss = ce_loss.mean()
 
-            td_error = model_predictions - q_value_targets
-            info["TD_Error"] = td_error.mean().data[0]
+            info = {}
 
-            # Update the priorities
-            if not self.args.density_priority:
-                self.replay.Update_Indices(indices, td_error.cpu().data.numpy(), no_pseudo_in_priority=self.args.count_td_priority)
-
-            # If using prioritised we need to weight the td_error
-            if self.args.prioritized and self.args.prioritized_is:
-                # print(td_error)
-                weights_tensor = torch.from_numpy(is_weights).float()
-                weights_tensor = Variable(weights_tensor)
-                if self.args.gpu:
-                    weights_tensor = weights_tensor.cuda()
-                # print(weights_tensor)
-                td_error = td_error * weights_tensor
-            l2_loss = (td_error).pow(2).mean()
-            info["Loss"] = l2_loss.data[0]
+            self.log("DQN/X_Entropy_Loss", ce_batch_loss.data[0], step=self.T)
 
             # Update
             self.optimizer.zero_grad()
-            l2_loss.backward()
+            ce_batch_loss.backward()
 
             # Taken from pytorch clip_grad_norm
             # Remove once the pip version it up to date with source
@@ -212,99 +245,5 @@ class DQN_Distribution_Agent:
             old_states = info["States"]
             new_states = old_states[0] * (self.args.batch_size - len(old_states))
             info["States"] = new_states
-
-        return info
-
-    def train_elig(self):
-        if self.T - self.target_sync_T > self.args.target:
-            self.sync_target_network()
-            self.target_sync_T = self.T
-
-        self.dqn.eval()
-
-        # TODO: Use a named tuple for experience replay
-        experiences, batch = self.replay.Sample_N_Eligibility_States(self.args.batch_size, self.args.gamma, self.args.num_states, self.args.gap)
-        columns = list(zip(*batch))
-
-        states = Variable(torch.from_numpy(np.array(columns[0])).float().transpose_(1, 3))
-        actions = Variable(torch.LongTensor(columns[1]))
-
-        elig_states = []
-        for seq in experiences:
-            for triple in seq:
-                elig_states.append(triple[0][0])
-        new_states = Variable(torch.from_numpy(np.array(elig_states)).float().transpose_(1, 3))
-
-        target_dqn_qvals = self.target_dqn(new_states).cpu()
-
-        q_targets = []
-
-        q_state_index = 0
-        for seq in experiences:
-
-            averaged_return = 0
-            lambda_ = 1
-            for triple in seq:
-                exp = triple[0]
-                accum_reward = triple[1]
-                # Clip
-                if accum_reward > 1:
-                    accum_reward = 1
-                if accum_reward < -1:
-                    accum_reward = -1
-
-                steps = triple[2]
-                terminal = exp.terminal
-
-                k_step_return = Variable(torch.FloatTensor([accum_reward] * self.args.actions))
-                if not terminal:
-                    k_step_return += (self.args.gamma ** steps) * target_dqn_qvals[q_state_index]
-                q_state_index += 1
-                averaged_return += lambda_ * k_step_return
-                lambda_ *= self.args.lambda_
-            # if len(seq) < 1:
-            #     # print(experiences)
-            #     # print(batch)
-            #     continue
-            averaged_return /= sum([self.args.lambda_ ** t for t in range(len(seq))])
-
-            max_q_val = averaged_return.data.max(0)[0][0]
-            # max_q_val = max_q_val.float()
-            q_targets.append(max_q_val)
-
-        q_value_targets = Variable(torch.from_numpy(np.array(q_targets)))
-        q_value_targets = q_value_targets.float()
-        # q_value_targets = q_targets
-
-        self.dqn.train()
-        if self.args.gpu:
-            actions = actions.cuda()
-            q_value_targets = q_value_targets.cuda()
-        model_predictions = self.dqn(states).gather(1, actions.view(-1, 1))
-        # print(q_value_targets)
-        # print(model_predictions)
-
-        info = {}
-
-        td_error = model_predictions - q_value_targets
-        info["TD_Error"] = td_error.mean().data[0]
-
-        # Update the priorities
-        # self.replay.Update_Indices(indices, td_error.cpu().data.numpy())
-
-        l2_loss = (td_error).pow(2).mean()
-        info["Loss"] = l2_loss.data[0]
-
-        # Update
-        self.optimizer.zero_grad()
-        l2_loss.backward()
-
-        # Taken from pytorch clip_grad_norm
-        # Remove once the pip version it up to date with source
-        gradient_norm = clip_grad_norm(self.dqn.parameters(), self.args.clip_value)
-        if gradient_norm is not None:
-            info["Norm"] = gradient_norm
-
-        self.optimizer.step()
 
         return info
