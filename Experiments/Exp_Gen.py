4f8b58d871b01c88d8d11e0211684fa5a322989d
Tabz <tabzraz@gmail.com>
2017-10-30 14:01:36 +0000

    Doom exps bigger count model

--- Experiments/Exp_Gen.py
+++ Experiments/Exp_Gen.py
@@ -1,33 +1,33 @@
 import sys
 from math import ceil
 
-# envs = ["DoomMaze-v0"] 
+envs = ["DoomMaze-v0"] 
 # envs = ["Thin-Maze-{}-Neg-v0".format(size) for size in [12]] 
-envs = ["Empty-Room-{}-v0".format(16, 18)]
+# envs = ["Empty-Room-{}-v0".format(16, 18)]
 DOOM = False
 if "Doom" in envs[0]:
     DOOM = True
 target_network = 1000
 
-eval_interval = 2
-vis_interval = 1000
+eval_interval = 100
+vis_interval = 100
 
 lrs = [0.0001] # 0.0001
 counts = [True]
 # cts_convs = [False]
 betas = [0.001] # 0.001
-t_maxs = [x * 1000 for x in [300]]
-cts_sizes = [12]
+t_maxs = [x * 1000 for x in [3000]]
+cts_sizes = [21]
 # num_seeds = 4
 num_seeds = 2
-epsilon_starts = [1, 1, 1]
-epsilon_finishs = [0.05, 0.05, 0.05]
-epsilon_steps = [1] + [x * 1000 for x in [100, 200]]
+epsilon_starts = [1]
+epsilon_finishs = [0.05]
+epsilon_steps = [1]
 batch_sizes = [(32, 1)]
-xp_replay_sizes = [x * 1000 for x in [300]]
+xp_replay_sizes = [x * 1000 for x in [250, 500]]
 stale_limits = [x * 1000 for x in [1000]]
-epsilon_scaling = [False]
-epsilon_decay = [0.9999]
+epsilon_scaling = [True]
+epsilon_decay = [0.9]
 
 n_steps = [1]
 variable_n_step = False
@@ -38,7 +38,7 @@ reward_clips = [-1]
 
 # state_action_modes = ["Plain", "Force", "Optimistic"]
 # state_action_modes = [None]
-optimism_scalers = [0, 0.01, 0.1, 1, 10]
+optimism_scalers = [0, 0.001, 0.01, 0.1]
 bandit_ps = [1/2] #[(1/4), (1/2), (1), (2)]
 state_action_modes = [None] + ["Optimistic" for _ in optimism_scalers]
 force_scalers = [0 for _ in state_action_modes]
