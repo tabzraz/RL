7bf1d6f1c248efb6b6aae7845a3729c41c4ac4b4
Tabz <tabzraz@gmail.com>
2017-10-31 01:03:53 +0000

    DoomMazeHard 5 10 step experiments

---
+++ Experiments/DoomMazeHard_5_10_Step__2017_10_31__Experiments/copy_logs.sh
@@ -0,0 +1,6 @@
+
+
+echo "Copying dgx1 Logs"
+ssh -t dgx1 "cp -r /data/dgx1/tabhid/Server_Logs/DoomMazeHard_5_10_Step__2017_10_31 /data/savitar/tabhid/Runs/Servers/DoomMazeHard_5_10_Step__2017_10_31"
+
+echo "Finished copying dgx1 Logs"
